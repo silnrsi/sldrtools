69e287c4f7431685bd5e9fe06923c50f69f0c37c
Martin Hosken <martin_hosken@sil.org>
2024-09-04 09:31:50 +0700

    Refactor basic test of collator to work

--- lib/sldr/collation.py
+++ lib/sldr/collation.py
@@ -450,7 +450,7 @@ class CollElement(object):
         self.key = basekey
         return basekey
 
-if __name__ == '__main__':
+def main():
     import sys
     coll = Collation()
     if len(sys.argv) > 1:
@@ -458,4 +458,7 @@ if __name__ == '__main__':
         alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
         alphabet += coll.keys()
         coll.minimise(alphabet)
-        print(coll.asICU())
+        print(coll.asICU(alphabet))
+
+if __name__ == '__main__':
+    main()
