ccb412d745d5edec98a6cf31cac4475f3af8750c
emily-roth <emily_roth@sil.org>
2026-04-06 12:16:14 -0400

    add info for lower priority fonts

--- scripts/sldrsupplementfonts.py
+++ scripts/sldrsupplementfonts.py
@@ -4,6 +4,8 @@ import os, re, argparse
 import logging
 import multiprocessing
 from sldr.ldml import Ldml, draftratings, iterate_files
+import xml.etree.cElementTree as et
+
 
 silns = {'sil' : "urn://www.sil.org/ldml/0.1" }
 gendraft = draftratings.get('generated', 5)
@@ -58,6 +60,7 @@ notLffUrls = {
     #note: these links seem to lead to a read-only version of the noto fonts repo. in future, may shift so that these instead direct to lff versions of these links
     "Noto Sans Telugu": "https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSansTelugu/NotoSansTelugu-Regular.ttf",
     "Noto Sans": "https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-Regular.ttf",
+    "Noto Serif": "https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSerif/NotoSerif-Regular.ttf",
     "Noto Serif Tibetan": "https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSerifTibetan/NotoSerifTibetan-Regular.ttf",
     "Noto Sans Thai": "https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSansThai/NotoSansThai-Regular.ttf",
     "Noto Sans Malayalam": "https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSansMalayalam/NotoSansMalayalam-Regular.ttf",
@@ -66,10 +69,20 @@ notLffUrls = {
     "Noto Serif Kannada": "https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSerifKannada/NotoSerifKannada-Regular.ttf"
 }
 
-fontgroups = {
-    # fonts that should always appear in a script's options
+mainfontgroups = {
+    # fonts that should always appear in a script's options, and should be either default or second to default
     # add more later, but be careful! Other SIL fonts might not have the same feature cross-compatibility that CGA have 
-    "Latn": ["Charis", "Gentium", "Andika"]
+    "Latn": ["Charis", "Gentium", "Andika"],
+    "Cyrl": ["Charis", "Gentium", "Andika"]
+}
+
+lowerfontgroups = {
+    # fonts that should also always appear, but should be lower tier than the ones listed in 'mainfontgroups'
+    # these are often non-sil fonts 
+    # for now, these should be without the default tag at all, 
+    # but in future, might get default tag with a specific, bigger number to indicate lower priority
+    "Latn": ["Noto Sans", "Noto Serif"],
+    "Cyrl": ["Noto Sans", "Noto Serif"]
 }
 
 
@@ -90,6 +103,7 @@ def processOneFile(fname, sldrPath):
         script = s.text
     elif i is not None:
         script = i.get("script")
+        
 
     # we have script of file
 
@@ -103,7 +117,7 @@ def processOneFile(fname, sldrPath):
         if font in lffUrls.keys():
             urlEl.text = "https://lff.api.languagetechnology.org/family/" + lffUrls[font]
             return False
-        elif font in notLffUrls.keys():
+        elif font in notLffUrls.keys():                
             urlEl.text = notLffUrls[font]
             return False
         else:
@@ -120,33 +134,66 @@ def processOneFile(fname, sldrPath):
 
     # we have handy dict of fonts and their features 
 
-    if script in fontgroups.keys():
-        neededfonts = fontgroups[script]
-        neededfeature = None
-        newfont = None
+    def _addMissingFonts(fontgroup, priority=None):
+        if script in fontgroup.keys():
+            neededfonts = fontgroup[script]
+            neededfeature = None
+            newfont = None
 
-        for font in neededfonts:
-            if font in fontList.keys():
-                neededfeature = fontList[font][1]
-                    #note: this will fall apart if multiple fonts are listed with different features, but that's not supposed to happen anyway?
-                
-                #this bit is in case multiple of these fonts are already in there, need to add the new priority
-                priority = fontList[font][0].get("types")
-                if priority is None:
-                    fontList[font][0].set("types", "default=2")
-                pass
-            else:
-                newfont = ldml.ensure_path('special/sil:external-resources/sil:font[@name="{}"][@types="default=2"][@features="{}"]'.format(font, neededfeature))[0]
-                _addLink(font, newfont)
-                fontList[font] = newfont
-                #we have now added the fonts that aren't there into the ldml file, along with the features and the priority
-                #we've also given it its link child node
-                #and we've added it to the font list we made earlier just in case (currently not necessary but again, just in case)
+            if neededfonts is not None:
+                for font in neededfonts:
+                    if font in fontList.keys():
+                        neededfeature = fontList[font][1]
+                            #note: this will fall apart if multiple fonts are listed with different features, but that's not supposed to happen anyway?
+                        
+                        #this bit is in case multiple of these fonts are already in there, need to add the new priority
+                        currentpriority = fontList[font][0].get("types")
+                        if currentpriority is None and priority is not None:
+                            fontList[font][0].set("types", "default={}".format(priority))
+                            # this is not equiped to handle multiple different priorities for different roles. 
+                            # it overrides the type rather than adds to the value inside it
+                            # if new roles get added, this whole function will need to be made smart enough to handle that
+                            # maybe add argument for the role (i.e. default, ui, etc) 
+                            # and when asking for currentpriority, actually read the text instead of just looking for if it exists
+                            # and determine if that SPECIFIC ROLE is present in text, and go from there
+                        pass
+                    else:
+                        if priority is not None and neededfeature is not None: 
+                            newfont = ldml.ensure_path('special/sil:external-resources/sil:font[@name="{}"][@types="default={}"][@features="{}"]'.format(font, priority, neededfeature))[0]
+                        elif neededfeature is not None:
+                            newfont = ldml.ensure_path('special/sil:external-resources/sil:font[@name="{}"][@features="{}"]'.format(font, neededfeature))[0]
+                        elif priority is not None:
+                            newfont = ldml.ensure_path('special/sil:external-resources/sil:font[@name="{}"][@types="default={}"]'.format(font, priority))[0]
+                        else: 
+                            newfont = ldml.ensure_path('special/sil:external-resources/sil:font[@name="{}"]'.format(font))[0]
+                        _addLink(font, newfont)
+                        fontList[font] = newfont
+                        #we have now added the fonts that aren't there into the ldml file, along with the features and the priority
+                        #we've also given it its link child node
+                        #and we've added it to the font list we made earlier just in case (currently not necessary but again, just in case)
+
+    _addMissingFonts(mainfontgroups, 2)
+    _addMissingFonts(lowerfontgroups)
+        # if we decide to give these fonts a specific priority instead of no default tag at all, add number to function arguments
 
     ldml.normalise()
     ldml.save_as(fpath)
     #and now it has saved the new stuff to the actual file and all is right in the world :)
 
+
+    # this is a hack to add in that comment <!--types="ui"--> in the child node of noto sans
+    with open(fpath, 'r+', encoding="utf8") as f:
+        lines = f.readlines()
+        f.seek(0)
+        f.truncate()
+        for line in lines:
+            oldline = line
+            if "Noto Sans" in line:
+                line = line + '\t\t\t\t<!--types="ui"-->\n'
+            if '<!--types="ui"-->' in oldline:
+                line = line.replace('\t\t\t\t<!--types="ui"-->\n', " ")
+            f.write(line)
+    
 
 parser = argparse.ArgumentParser()
 parser.add_argument("indir",help="Root of SLDR file tree")
